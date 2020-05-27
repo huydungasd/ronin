dfd0084375d86017d41fb56815cd774e8cb1206c
huydungasd <huydungasd@gmail.com>
2020-05-27 11:07:42 +0200

    _

--- source/create_train_list.py
+++ source/create_train_list.py
@@ -1,47 +1,47 @@
 path = '../'
-with open("train_list.txt","w") as f:
-    for i in range(65):
-        f.write(f'{path}data_deep/data1/imu/{i}.csv\n')
-    for i in range(85):
-        f.write(f'{path}data_deep/data2/imu/{i}.csv\n')
-    for i in range(90):
-        f.write(f'{path}data_deep/data3/imu/{i}.csv\n')
-    for i in range(135):
+with open("../lists/train_list.txt","w") as f:
+    # for i in range(65):
+    #     f.write(f'{path}data_deep/data1/imu/{i}.csv\n')
+    # for i in range(85):
+    #     f.write(f'{path}data_deep/data2/imu/{i}.csv\n')
+    # for i in range(90):
+    #     f.write(f'{path}data_deep/data3/imu/{i}.csv\n')
+    for i in range(125):
         f.write(f'{path}data_deep/data4/imu/{i}.csv\n')
-    for i in range(135):
+    for i in range(125):
         f.write(f'{path}data_deep/data5/imu/{i}.csv\n')
-    for i in range(29):
+    for i in range(27):
         f.write(f'{path}data_deep/data6/imu/{i}.csv\n')
-    for i in range(105):
+    for i in range(95):
         f.write(f'{path}data_deep/data7/imu/{i}.csv\n')
-    for i in range(130):
+    for i in range(120):
         f.write(f'{path}data_deep/data8/imu/{i}.csv\n')
 
 with open("val_list.txt","w") as f:
-    for i in range(65, 69):
-        f.write(f'{path}data_deep/data1/imu/{i}.csv\n')
-    for i in range(85, 90):
-        f.write(f'{path}data_deep/data2/imu/{i}.csv\n')
-    for i in range(90, 95):
-        f.write(f'{path}data_deep/data3/imu/{i}.csv\n')
-    for i in range(135, 143):
+    # for i in range(65, 69):
+    #     f.write(f'{path}data_deep/data1/imu/{i}.csv\n')
+    # for i in range(85, 90):
+    #     f.write(f'{path}data_deep/data2/imu/{i}.csv\n')
+    # for i in range(90, 95):
+    #     f.write(f'{path}data_deep/data3/imu/{i}.csv\n')
+    for i in range(125, 143):
         f.write(f'{path}data_deep/data4/imu/{i}.csv\n')
-    for i in range(135, 143):
+    for i in range(125, 143):
         f.write(f'{path}data_deep/data5/imu/{i}.csv\n')
-    for i in range(29, 30):
+    for i in range(27, 30):
         f.write(f'{path}data_deep/data6/imu/{i}.csv\n')
-    for i in range(105, 113):
+    for i in range(95, 113):
         f.write(f'{path}data_deep/data7/imu/{i}.csv\n')
-    for i in range(130, 137):
+    for i in range(120, 137):
         f.write(f'{path}data_deep/data8/imu/{i}.csv\n')
 
 with open("test_list.txt","w") as f:
-    for i in range(69, 73):
-        f.write(f'{path}data_deep/data1/imu/{i}.csv\n')
-    for i in range(90, 95):
-        f.write(f'{path}data_deep/data2/imu/{i}.csv\n')
-    for i in range(95, 100):
-        f.write(f'{path}data_deep/data3/imu/{i}.csv\n')
+    # for i in range(69, 73):
+    #     f.write(f'{path}data_deep/data1/imu/{i}.csv\n')
+    # for i in range(90, 95):
+    #     f.write(f'{path}data_deep/data2/imu/{i}.csv\n')
+    # for i in range(95, 100):
+    #     f.write(f'{path}data_deep/data3/imu/{i}.csv\n')
     for i in range(143, 151):
         f.write(f'{path}data_deep/data4/imu/{i}.csv\n')
     for i in range(143, 151):
@@ -50,5 +50,5 @@ with open("test_list.txt","w") as f:
         f.write(f'{path}data_deep/data6/imu/{i}.csv\n')
     for i in range(113, 120):
         f.write(f'{path}data_deep/data7/imu/{i}.csv\n')
-    for i in range(137, 145):
+    for i in range(137, 144):
         f.write(f'{path}data_deep/data8/imu/{i}.csv\n')
